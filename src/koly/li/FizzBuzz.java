0e4e66bfbdcc157255a5562f1831cc65d3bb2d3c
kolyjjj <me.koly.li@gmail.com>
2013-02-17 21:09:39 +0800

    Introduced the stringbuilder, and commented the codes

--- src/koly/li/FizzBuzz.java
+++ src/koly/li/FizzBuzz.java
@@ -2,16 +2,20 @@ package koly.li;
 
 public class FizzBuzz {
     public String run(int inputNumber) {
-        if (isDivisibleByThree(inputNumber) && isDivisibleByFive(inputNumber)) {
-            return "FizzBuzz";
-        }
+        StringBuilder result = new StringBuilder();
+//        if (isDivisibleByThree(inputNumber) && isDivisibleByFive(inputNumber)) {
+//            return "FizzBuzz";
+//        }
         if (isDivisibleByThree(inputNumber)) {
-            return "Fizz";
+            //return "Fizz";
+            result.append("Fizz");
         }
         if (isDivisibleByFive(inputNumber)) {
-            return "Buzz";
+//            return "Buzz";
+            result.append("Buzz");
         }
-        return null;
+//        return null;
+        return result.toString();
     }
 
     private boolean isDivisibleByFive(int inputNumber) {
