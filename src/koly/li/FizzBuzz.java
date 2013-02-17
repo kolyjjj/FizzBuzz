61659661e3f75d5699e80baef111f0c1bf1815af
kolyjjj <me.koly.li@gmail.com>
2013-02-17 21:03:01 +0800

    This is the modified bad code

--- src/koly/li/FizzBuzz.java
+++ src/koly/li/FizzBuzz.java
@@ -1,16 +1,16 @@
 package koly.li;
 
 public class FizzBuzz {
-    public String run(int num) {
-        if (num % 3 == 0 && num % 5 == 0 ){
-            return "FizzBuzz";
-        }
-        if (num % 3 == 0){
-            return "Fizz";
-        }
-        if (num % 5 == 0){
-            return "Buzz";
-        }
-        return null;
+    public String yunxing(int i) {
+    if (i % 3 == 0 && i % 5 == 0 ){
+        return "FizzBuzz";
+    }
+    if (i % 3 == 0){
+        return "Fizz";
+    }
+    if (i % 5 == 0){
+        return "Buzz";
+    }
+    return null;
     }
 }
