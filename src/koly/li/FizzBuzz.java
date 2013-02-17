d4f682eee308e3df781767635169f4bcd7f30561
kolyjjj <me.koly.li@gmail.com>
2013-02-17 21:18:42 +0800

    move the parameter to the class field

--- src/koly/li/FizzBuzz.java
+++ src/koly/li/FizzBuzz.java
@@ -1,29 +1,33 @@
 package koly.li;
 
 public class FizzBuzz {
+
+    private int inputNumber;
+
     public String run(int inputNumber) {
+        this.inputNumber = inputNumber;
         StringBuilder result = new StringBuilder();
-        if (isDivisibleByThree(inputNumber)) {
+        if (isDivisibleByThree()) {
             result.append("Fizz");
         }
-        if (isDivisibleByFive(inputNumber)) {
+        if (isDivisibleByFive()) {
             result.append("Buzz");
         }
-        if (isDivisibleByTwo(inputNumber)){
+        if (isDivisibleByTwo()){
             result.append("Hizz");
         }
         return result.toString();
     }
 
-    private boolean isDivisibleByTwo(int inputNumber) {
+    private boolean isDivisibleByTwo() {
         return inputNumber % 2 == 0;
     }
 
-    private boolean isDivisibleByFive(int inputNumber) {
+    private boolean isDivisibleByFive() {
         return inputNumber % 5 == 0;
     }
 
-    private boolean isDivisibleByThree(int inputNumber) {
+    private boolean isDivisibleByThree() {
         return inputNumber % 3 == 0;
     }
 }
