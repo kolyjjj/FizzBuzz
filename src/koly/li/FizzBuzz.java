89880775fa3daa4523be1f0b503bcc487d39fcae
kolyjjj <me.koly.li@gmail.com>
2013-02-17 22:25:16 +0800

    Extract lines of codes to methods

--- src/koly/li/FizzBuzz.java
+++ src/koly/li/FizzBuzz.java
@@ -6,18 +6,25 @@ import java.util.List;
 public class FizzBuzz {
     public String run(int inputNumber) {
         StringBuilder result = new StringBuilder();
-        List<Divisible> divisibleObjects = new ArrayList<Divisible>();
-        divisibleObjects.add(new DivisibleByThree());
-        divisibleObjects.add(new DivisibleByFive());
-        divisibleObjects.add(new DivisibleByTwo());
+        List<Divisible> divisibleObjects = initilizeDivisibleObjects();
+        appendStrings(inputNumber, result, divisibleObjects);
+        return result.toString();
+    }
 
+    private void appendStrings(int inputNumber, StringBuilder result, List<Divisible> divisibleObjects) {
         for (Divisible d : divisibleObjects){
             if (d.by(inputNumber)){
                 result.append(d.getSpecifiedString());
             }
-
         }
-        return result.toString();
+    }
+
+    private List<Divisible> initilizeDivisibleObjects() {
+        List<Divisible> divisibleObjects = new ArrayList<Divisible>();
+        divisibleObjects.add(new DivisibleByThree());
+        divisibleObjects.add(new DivisibleByFive());
+        divisibleObjects.add(new DivisibleByTwo());
+        return divisibleObjects;
     }
 
 }
