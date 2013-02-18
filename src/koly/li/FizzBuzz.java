a1afd18a44919ef282ff8140bf3b44923a463249
kolyjjj <me.koly.li@gmail.com>
2013-02-18 10:31:03 +0800

    Really bad codes

--- src/koly/li/FizzBuzz.java
+++ src/koly/li/FizzBuzz.java
@@ -1,16 +1,25 @@
 package koly.li;
 
 public class FizzBuzz {
-    public String run(int num) {
-        if (num % 3 == 0 && num % 5 == 0 ){
+    public boolean shuruhefa(int x){
+        if (x < 0)
+             return false;
+        return true;
+    }
+    public String yunxing(int x) {
+        //return FizzBuzz
+        if (x % 3 == 0 && x % 5 == 0 ){
             return "FizzBuzz";
         }
-        if (num % 3 == 0){
+        //return Fizz
+        if (x % 3 == 0){
             return "Fizz";
         }
-        if (num % 5 == 0){
+        //return Buzz
+        if (x % 5 == 0){
             return "Buzz";
         }
-        return null;
+        //return empty string
+        return "";
     }
 }
--- test/koly/li/FizzBuzzTest.java
+++ test/koly/li/FizzBuzzTest.java
@@ -16,24 +16,26 @@ public class FizzBuzzTest {
     }
 
     @Test
+    public void should_return_empty_string_when_input_is_1() throws Exception {
+        assertThat(fizzBuzz.yunxing(1), is(""));
+    }
+
+    @Test
     public void should_return_fizz_when_input_is_divisible_by_3() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz.run(3), is("Fizz"));
+        assertThat(fizzBuzz.yunxing(3), is("Fizz"));
     }
 
     @Test
     public void should_return_fizz_when_input_is_divisible_by_6() throws Exception {
-        assertThat(fizzBuzz.run(3), is("Fizz"));
+        assertThat(fizzBuzz.yunxing(3), is("Fizz"));
     }
 
     @Test
     public void should_return_buzz_when_input_is_divisible_by_5() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz.run(10), is("Buzz"));
+        assertThat(fizzBuzz.yunxing(10), is("Buzz"));
     }
     @Test
     public void should_return_fizzbuzz_when_input_is_divisible_both_by_3_and_5() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz.run(15), is("FizzBuzz"));
+        assertThat(fizzBuzz.yunxing(15), is("FizzBuzz"));
     }
 }
