6774a91b00fc286476e0c65c17d778da5ffee5e0
kolyjjj <me.koly.li@gmail.com>
2013-02-18 14:52:27 +0800

    add the identation to make the code worse

--- src/koly/li/FizzBuzz.java
+++ src/koly/li/FizzBuzz.java
@@ -2,24 +2,21 @@ package koly.li;
 
 public class FizzBuzz {
     public boolean shuruhefa(int x){
-        if (x < 0)
-             return false;
-        return true;
+    if (x < 0)
+         return false;
+    return true;
     }
     public String yunxing(int x) {
         //return FizzBuzz
-        if (x % 3 == 0 && x % 5 == 0 ){
-            return "FizzBuzz";
-        }
-        //return Fizz
-        if (x % 3 == 0){
-            return "Fizz";
-        }
-        //return Buzz
-        if (x % 5 == 0){
-            return "Buzz";
-        }
-        //return empty string
-        return "";
+    if (x % 3 == 0 && x % 5 == 0 ){
+        return "FizzBuzz";}
+    //return Fizz
+    if (x % 3 == 0){
+        return "Fizz";}
+    //return Buzz
+    if (x % 5 == 0){
+        return "Buzz";}
+    //return empty string
+    return "";
     }
 }
